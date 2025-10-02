b62a5fa482bb294e51da879eedb74b84d8117d30
Gokul-Gopi <gokulg1412@gmail.com>
2025-10-02 09:32:17 +0000

    feat(web): movie card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,5 @@
-import MovieCard from "@/components/MovieCard";
-
 const Page = () => {
-  return (
-    <div className=" grid grid-cols-4 gap-4 p-6">
-      {Array.from({ length: 7 }).map(() => (
-        <MovieCard
-          key={2}
-          title="Avengers"
-          description="Lorem ipsum dolor sit amet"
-          publishedOn="2020"
-          poster="https://plus.unsplash.com/premium_photo-1755889381455-ba9accf067cc?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dx"
-        />
-      ))}
-    </div>
-  );
+  return <div>Hello</div>;
 };
 
 export default Page;
